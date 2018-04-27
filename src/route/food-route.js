fd689adfcd2e60ea321f49ffb01d12d71e0397ff
dahby <dstoll243@gmail.com>
2018-04-26 17:24:36 -0700

    refactoring FOOD ROUTE functions but commenting out because they arent all working

--- src/route/food-route.js
+++ src/route/food-route.js
@@ -28,26 +28,17 @@ module.exports = function routeFood(router) {
     logger.log(logger.INFO, 'FOOD-ROUTE: GET /api/v1/food');
     if (!req.url.query.id) {
       response.sendError(res, 404, 'Your request requires an id');
-      // res.writeHead(400, { 'Content-Type': 'text/plain' });
-      // res.write('Your request requires an id');
-      // res.end();
       return undefined;
     }
-    
+
     storage.fetchOne('Food', req.url.query.id)
       .then((item) => {
         response.sendJSON(res, 200, item);
-        // res.writeHead(200, { 'Content-Type': 'application/json' });
-        // res.write(JSON.stringify(item));
-        // res.end();
         return undefined;
       })
       .catch((err) => {
         logger.log(logger.ERROR, err, JSON.stringify(err));
         response.sendError(res, 404, 'Resource not found');
-        // res.writeHead(404, { 'Content-Type': 'text/plain' });
-        // res.write('Resource not found');
-        // res.end();
         return undefined;
       });
     return undefined;
@@ -73,55 +64,61 @@ module.exports = function routeFood(router) {
   //     });
   //   return undefined;
   // });
-  router.put('/api/v1/food', (req, res) => {
-    logger.logg(logger.INFO, 'FOOD-ROUTE: UPDATE food');
-    if (!req.body.id) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.write('Update request requires ID');
-      res.end();
-      return undefined;
-    }
-    try {
-      storage.update('Food', req.url.query.id)
-        .then((food) => {
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.write(JSON.stringify(food));
-          res.end();
-          return undefined;
-        });
-    } catch (err) {
-      logger.log(logger.ERROR, `FOOD-ROUTE: Bad request ${err}`);
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write('PUT Resource not found');
-      res.end();
-      return undefined;
-    }
-    return undefined;
-  });
+  // router.put('/api/v1/food', (req, res) => {
+  //   logger.logg(logger.INFO, 'FOOD-ROUTE: UPDATE food');
+  //   if (!req.body.id) {
+  //     response.sendError(res, 200, 'Update request requires id');
+  //     // res.writeHead(400, { 'Content-Type': 'text/plain' });
+  //     // res.write('Update request requires ID');
+  //     // res.end();
+  //     return undefined;
+  //   }
+  //   try {
+  //     storage.update('Food', req.url.query.id)
+  //       .then((food) => {
+  //         response.sendJSON(res, 200, food);
+  //         // res.writeHead(200, { 'Content-Type': 'application/json' });
+  //         // res.write(JSON.stringify(food));
+  //         // res.end();
+  //         return undefined;
+  //       });
+  //   } catch (err) {
+  //     logger.log(logger.ERROR, `FOOD-ROUTE: Bad request ${err}`);
+  //     response.sendError(res, 404, 'PUT Resource not found');
+  //     // res.writeHead(404, { 'Content-Type': 'text/plain' });
+  //     // res.write('PUT Resource not found');
+  //     // res.end();
+  //     return undefined;
+  //   }
+  //   return undefined;
+  // });
 
-  router.delete('/api/v1/food', (req, res) => {
-    logger.log(logger.INFO, 'FOOD-ROUTE: DEL /api/v1/food');
-    if (!req.url.query.id) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.write('Delete request requires ID');
-      res.end();
-      return undefined;
-    }
-    storage.delete('Food', req.url.query.id)
-      .then((food) => {
-        logger.log(logger.INFO, `FOOD-ROUTE: DEL ${food}`);
-        res.writeHead(204, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify(food));
-        res.end();
-        return undefined;
-      })
-      .catch((err) => {
-        logger.log(logger.ERROR, `FOOD-ROUTE: Bad request ${err}`);
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.write('DEL resource bad request');
-        res.end();
-        return undefined;
-      });
-    return undefined;
-  });
+  // router.delete('/api/v1/food', (req, res) => {
+  //   logger.log(logger.INFO, 'FOOD-ROUTE: DEL /api/v1/food');
+  //   if (!req.url.query.id) {
+  //     response.sendError(res, 400, 'Delete request requires id');
+  //     // res.writeHead(400, { 'Content-Type': 'text/plain' });
+  //     // res.write('Delete request requires ID');
+  //     // res.end();
+  //     return undefined;
+  //   }
+  //   storage.delete('Food', req.url.query.id)
+  //     .then((food) => {
+  //       logger.log(logger.INFO, `FOOD-ROUTE: DEL ${food}`);
+  //       response.sendJSON(res, 204, food);
+  //       // res.writeHead(204, { 'Content-Type': 'application/json' });
+  //       // res.write(JSON.stringify(food));
+  //       // res.end();
+  //       return undefined;
+  //     })
+  //     .catch((err) => {
+  //       logger.log(logger.ERROR, `FOOD-ROUTE: Bad request ${err}`);
+  //       response.sendError(res, 404, 'DEL resource bad request');
+  //       // res.writeHead(404, { 'Content-Type': 'text/plain' });
+  //       // res.write('DEL resource bad request');
+  //       // res.end();
+  //       return undefined;
+  //     });
+  //   return undefined;
+  // });
 };
